d1d62a6fa98e9e7f4e357623cc77f50f02b3198a
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2022-02-11 20:25:43 +0700

    update

--- src/models/User.js
+++ src/models/User.js
@@ -4,8 +4,6 @@ const Schema = mongoose.Schema;
 const User = new Schema({
     name:String,
     email:String,
-    username:{type:String,unique:true},
-    password:{type:String},
     photoURL:String,
     uid:String,
     listSongLove:[{type:Schema.Types.ObjectId,ref:'Song'}]
